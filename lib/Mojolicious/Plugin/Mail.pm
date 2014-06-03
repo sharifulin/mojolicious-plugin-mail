5fa529f5a6ba2a3dad509939dbbf428f1604de35
Anatoly Sharifulin <sharifulin@ostrovok.ru>
2014-06-03 18:12:34 +0400

    update version to v1.3

--- lib/Mojolicious/Plugin/Mail.pm
+++ lib/Mojolicious/Plugin/Mail.pm
@@ -10,7 +10,7 @@ use constant FROM     => 'test-mail-plugin@mojolicio.us';
 use constant CHARSET  => 'UTF-8';
 use constant ENCODING => 'base64';
 
-our $VERSION = '1.2';
+our $VERSION = '1.3';
 
 has conf => sub { +{} };
 
